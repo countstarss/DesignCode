029c404b52a3ce58d1932d7d9a3e11b887aaffe1
Luke king <lukeking404@icloud.com>
2024-04-18 07:58:53 +0800

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by 王佩豪 on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
